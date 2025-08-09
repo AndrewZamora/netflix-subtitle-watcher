b0369dd073ea125b56bc724cfa1a0def17838f4b
AndrewZamora <andrewmichaelzamora@gmail.com>
2025-08-09 23:26:39 +0900

    used chrome storage

--- src/background.js
+++ src/background.js
@@ -1,41 +1,22 @@
 function storeQuery(query) {
-  let data = {
-    'hardcoded': {
-      query
-    }
-  }
-
-  // chrome.storage.local.get('hardcoded', () => console.log("stored data"))
-  // chrome.storage.local.set({ netflix: JSON.stringify(data) }, () => console.log("stored data"))
+  chrome.storage.sync.set({ [query]: { definition: '' } }, () => console.log("stored data"));
 }
 
-chrome.contextMenus.create({
-  id: "searchOnJisho",
-  title: "Search on Jisho.org",
-  contexts: ["selection"]
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("installed");
+  chrome.contextMenus.create({
+    id: "searchOnJisho",
+    title: "Search on Jisho.org",
+    contexts: ["selection"]
+  }, () => { console.log("created context menu item") });
 });
 
-// chrome.contextMenus.create({
-//   id: "translateWithGoogle",
-//   title: "Translate with Google (Japanese to English)",
-//   contexts: ["selection"]
-// });
-
-// Handle clicks on the context menu item
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "searchOnJisho" && info.selectionText) {
+    console.table(info)
     const query = encodeURIComponent(info.selectionText.trim());
     const url = `https://jisho.org/search/${query}`;
     chrome.tabs.create({ url });
-    console.log('test')
+    storeQuery(info.selectionText.trim())
   }
-  // if (info.menuItemId === "translateWithGoogle" && info.selectionText) {
-  //   const query = encodeURIComponent(info.selectionText.trim());
-  //   const url = `https://translate.google.com/?sl=ja&tl=en&text=${query}&op=translate`;
-  //   chrome.tabs.create({ url });
-  // }
-  // TODO:
-  // if (info.menuItemId && info.selectionText) {
-  //   storeQuery(info.selectionText.trim())
-  // }
 });
--- src/content.js
+++ src/content.js
@@ -1,4 +1,5 @@
 import { createSubtitleDisplay } from "./subtitles/createSubtitleDisplay";
 import { startSubtitleObserver } from "./subtitles/startSubtitleObserver";
+
 createSubtitleDisplay();
 const observer = startSubtitleObserver();
